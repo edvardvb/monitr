e5ea49f380c5e6d0bd75d5110f5405bdd09dc314
svennhv <svennhv@gmail.com>
2016-03-09 14:22:20 +0100

    create status controller, fix routes for status

---
+++ monitr/app/controllers/status_controller.rb
@@ -0,0 +1,11 @@
+class StatusController < ApplicationController
+
+  def recieve_post # function for receiving post requests
+    puts 'recieved, wohoo! :) ;) <3'  
+  end
+
+  def update # function for updating redis database
+
+  end
+
+end
