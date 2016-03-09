106b505f68409e9c02b6f2e6600ee72fa1dd80ad
svennhv <svennhv@gmail.com>
2016-03-09 15:54:51 +0100

    finalize status controller logic

--- monitr/app/controllers/status_controller.rb
+++ monitr/app/controllers/status_controller.rb
@@ -1,14 +1,23 @@
 class StatusController < ApplicationController
 
+  # temporary list?  - move to redis?
+  @@status_variables = ["engine_speed", "vehicle_speed"]
+
   def recieve_post # function for receiving post requests
     puts 'recieved, wohoo! :) ;) <3'
-    puts params
-    puts params[:name]
-    a = false
+
+    for var in @@status_variables
+      if params[:name] == var
+        update(var)
+        puts "updated:"
+        puts $redis.get var
+      end
+    end
+
   end
 
-  def update # function for updating redis database
-
+  def update(var) # function for updating redis database
+      $redis.set var, params[:value]
   end
 
 end
